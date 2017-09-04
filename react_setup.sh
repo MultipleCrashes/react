e5944b32066cc357f3968c0f7c1d3da06c988b07
MultipleCrashes <inimitableharish@gmail.com>
2017-09-04 21:18:17 +0530

    setup modified

--- react_setup.sh
+++ react_setup.sh
@@ -1,10 +1,18 @@
 sudo apt-get install npm
+mkdir reactApp
+cd reactApp/
+npm init
+sudo npm install react --save
+sudo npm install react-dom --save 
+sudo npm install webpack --save 
+sudo npm install webpack-dev-server --save 
 sudo npm install -g babel
 sudo npm install -g babel-cli
 sudo npm install babel-core
 sudo npm install babel-loader 
 sudo npm install babel-preset-react 
 sudo npm install babel-preset-es2015
-mkdir reactApp
-cd reactApp/
-npm init
+sudo touch index.html 
+sudo touch App.jsx 
+sudo touch main.js 
+sudo touch webpack.config.js
