ebc233826f0d13af697b8edf4d053e577c0c809f
MultipleCrashes <inimitableharish@gmail.com>
2017-09-04 21:56:58 +0530

    react package

--- react_setup.sh
+++ react_setup.sh
@@ -1,11 +1,13 @@
 sudo apt-get install npm
+sudo apt-get install nodejs
 mkdir reactApp
 cd reactApp/
 npm init
 sudo npm install react --save
 sudo npm install react-dom --save 
 sudo npm install webpack --save 
-sudo npm install webpack-dev-server --save 
+sudo npm install sync-exec --save 
+sudo npm install webpack-dev-server --save
 sudo npm install -g babel
 sudo npm install -g babel-cli
 sudo npm install babel-core
