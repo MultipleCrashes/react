393b66495e151594c6572d07c4b015742cd2a51e
MultipleCrashes <inimitableharish@gmail.com>
2017-09-04 21:07:55 +0530

    react setup file

---
+++ react_setup.sh
@@ -0,0 +1,10 @@
+sudo apt-get install npm
+sudo npm install -g babel
+sudo npm install -g babel-cli
+sudo npm install babel-core
+sudo npm install babel-loader 
+sudo npm install babel-preset-react 
+sudo npm install babel-preset-es2015
+mkdir reactApp
+cd reactApp/
+npm init
